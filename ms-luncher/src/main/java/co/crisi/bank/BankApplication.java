6323c7449c35dfcd207036d7502a220e74c325d4
Cristian Sánchez <harmaharcri@hotmail.com>
2023-04-09 07:45:03 -0500

    Refactor and improve the maven config
    also added a pr template and added the scan base packages

--- ms-luncher/src/main/java/co/crisi/bank/BankApplication.java
+++ ms-luncher/src/main/java/co/crisi/bank/BankApplication.java
@@ -3,7 +3,7 @@ package co.crisi.bank;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "co.crisi.bank")
 public class BankApplication {
 
     public static void main(String[] args) {
